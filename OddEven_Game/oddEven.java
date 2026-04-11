be1ce2f3be3bc63986d3246aeee724c95bc9dc6b
xshyrz <shaineyroseselgas@gmail.com>
2026-04-11 14:30:05 +0800

    first commit

---
+++ OddEven_Game/oddEven.java
@@ -0,0 +1,24 @@
+public class oddEven extends javax.swing.JFrame {
+        
+    }
+
+    @SuppressWarnings("unchecked")
+
+    private void initComponents() {
+
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton even;
+    private javax.swing.JButton exit;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JLabel label;
+    private javax.swing.JLabel number;
+    private javax.swing.JButton odd;
+    private javax.swing.JLabel score;
+    // End of variables declaration//GEN-END:variables
+}
